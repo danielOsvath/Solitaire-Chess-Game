6ac1bbbfa1a6b96365d8e2153b229e1fcb44cbac
Daniel Osvath Londono <o.dani8@yahoo.com>
2016-11-23 01:04:35 -0500

    Created config class

---
+++ src/backtracking/SoltrChessConfig.java
@@ -0,0 +1,33 @@
+package backtracking;
+/*
+ * SoltrChessConfig.java
+ * Created by Daniel Osvath Londono on 11/23/16.
+ */
+
+
+import java.util.Collection;
+
+/**
+ * Class Purpose
+ *
+ * @author Daniel Osvath Londono
+ * @author Nathan Cassata
+ * @since 11/23/16
+ */
+public class SoltrChessConfig implements Configuration{
+
+    @Override
+    public Collection<Configuration> getSuccessors() {
+        return null;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public boolean isGoal() {
+        return false;
+    }
+}
