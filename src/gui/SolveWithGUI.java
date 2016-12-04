f17418a944db416fa2b601a64438ac07caef335c
Daniel Osvath Londono <o.dani8@yahoo.com>
2016-12-04 12:07:49 -0500

    Created SolveWithGUI class, a thread class that will be necessary for the GUI.

---
+++ src/gui/SolveWithGUI.java
@@ -0,0 +1,59 @@
+package gui;
+/*
+ * SolveWithGUI.java
+ * Created by Daniel Osvath Londono on 12/4/16.
+ */
+
+
+import backtracking.Configuration;
+import javafx.scene.control.Label;
+import model.SoltrChessModel;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class Purpose
+ *
+ * @author Daniel Osvath Londono
+ * @author Nathan Cassata
+ * @since 12/4/16
+ */
+public class SolveWithGUI extends Thread {
+
+    /* */
+    private SoltrChessModel model;
+
+    /**
+     *
+     * @param model
+     * @param messagefield
+     */
+    public SolveWithGUI(SoltrChessModel model, Label messagefield){
+        this.model = model;
+    }
+
+    /**
+     *
+     */
+    public void run(){
+
+        List<Configuration> steps = this.model.getSolveSteps();
+
+        if(steps != null){
+
+            for (int curStep = 1; curStep < steps.size(); curStep++){
+
+                model.setBoard(steps.get(curStep).getBoard());
+
+                try{
+                    TimeUnit.SECONDS.sleep(1);
+                }catch (InterruptedException e){
+
+                }
+
+            }
+
+        }
+    }
+}
